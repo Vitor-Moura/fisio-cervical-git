912904c82f94b6f647e16956a3994873853cbf95
Vitor-Moura <vitor.moura@bandtec.com.br>
2021-11-04 19:12:44 -0300

    Crianção da modelagem de dados e atualização do script

--- script-fisiocervical.sql
+++ script-fisiocervical.sql
@@ -2,24 +2,39 @@ CREATE DATABASE fisiocervical;
 
 USE fisiocervical;
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE fisioterapeuta (
+	idFisioterapeuta INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
 	senha VARCHAR(50)
 );
 
-CREATE TABLE fisioterapeuta (
+CREATE TABLE exercicios (
+	idExercicios INT PRIMARY KEY AUTO_INCREMENT,
+	preTreino INT,
+	posTreino INT
+);
+
+CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50)
-);
+	senha VARCHAR(50),
+    fkFisioterapeuta INT,
+	fkExercicios INT,
+	FOREIGN KEY (fkFisioterapeuta) REFERENCES fisioterapeuta(idFisioterapeuta),
+	FOREIGN KEY (fkExercicios) REFERENCES exercicios(idExercicios));
+
 
 
 select * from usuario;
 
 select * from fisioterapeuta;
 
---  insert into usuario values 
--- (null, 'Vitor', 'vitor@gmail.com', '1234567');
+
+delete from usuario where usuario.id = 1;
+delete from fisioterapeuta where idFisioterapeuta = 1;
+
+
+drop table fisioterapeuta;
+drop table usuario;
